48c23d515cc00bc1ba9d3ee44a3414fd91863727
Snorro1998 <tjaard.van.verseveld@gmail.com>
2019-04-26 15:13:48 +0200

    Hoofdmenu toegevoegd

---
+++ BPW_GameJam/Assets/Scripts/StartButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void loadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
