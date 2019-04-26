c48a100663fcf4433f83b8bc7fea0d6d026b6672
Chantal <c_voort1992@hotmail.com>
2019-04-26 13:22:43 +0200

    Adding char controller layout Adding controls

---
+++ BPW_GameJam/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour{
+
+    void Update(){
+        if(Input.GetButtonDown("Jump")) {
+			Jump();
+		}
+
+		if(Input.GetButtonDown("Slide")) {
+			Slide();
+		}
+
+		if(Input.GetButton("LeftHand")) {
+			LeftHand();
+		}
+
+		if(Input.GetButton("RightHand")) {
+			RightHand();
+		}
+	}
+
+	//jump
+	void Jump() {
+	}
+
+	//slide
+	void Slide() {
+	}
+
+	//sticks out left hand
+	void LeftHand() {
+	}
+
+	//sticks out right hand
+	void RightHand() {
+	}
+}
